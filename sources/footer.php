c51912647505c5ab703f69c953e2dd87694971b3
Maniack Crudelis <maniack_crudelis@yahoo.fr>
2015-11-22 14:37:01 +0100

    MAJ script + backup

--- sources/footer.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$tpl->assign('executionTime',number_format(microtime(true)-$start,3));
-$html = $tpl->draw($view);
-?>
--- sources/header.php
+++
@@ -1,8 +0,0 @@
-<?php
-if(!file_exists('constant.php')) {
-    header('location: install.php');
-    exit();
-}
-require_once('common.php');
-
-?>
